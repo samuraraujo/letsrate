425c507249bdb292edde7f661e36c9e52203acdf
Samur Araujo, PhD. <samuraraujo@gmail.com>
2014-04-23 09:42:06 -0300

    Update helpers.rb
    Add support to change rate

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -9,12 +9,12 @@ module Helpers
 
     disable_after_rate = options[:disable_after_rate] || true
 
-    readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
+    #readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
 
     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
-                "data-readonly" => readonly,
+                #"data-readonly" => readonly,
                 "data-star-count" => star
   end
 
@@ -26,15 +26,15 @@ module Helpers
 
     disable_after_rate = options[:disable_after_rate] || false
 
-    readonly=false
-    if disable_after_rate
-      readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
-    end
+   # readonly=false
+    #if disable_after_rate
+     # readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
+    #end
 
     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => stars,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
-                "data-readonly" => readonly,
+                #"data-readonly" => readonly,
                 "data-star-count" => stars
   end
 
