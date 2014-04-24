eee2242fd83163705989e58d75959aede6c7b4ad
Samur Araujo, PhD. <samuraraujo@gmail.com>
2014-04-23 22:32:58 -0300

    Update helpers.rb

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -11,7 +11,7 @@ module Helpers
 
     #readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
 
-    content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
+    content_tag :span, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
                 #"data-readonly" => readonly,
